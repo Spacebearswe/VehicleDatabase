87cf1230038f6299876acebcd673259b8f246ba5
Stefan <mornhag@yahoo.se>
2022-02-06 11:18:28 +0100

    Add project files.

---
+++ Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleDatabase.Models
+{
+    public class Vehicle
+    {
+        public long Id { get; set; }
+        public string ModelName { get; set; }
+        public string PlateNumber { get; set; }
+        public string VIN { get; set; }
+    }
+}
